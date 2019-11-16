8283cf38f82177649a480d08903e87c2d448bbce
Ionel Campan <campan.ionel@gmail.com>
2019-11-16 10:15:43 +0200

    Updates

---
+++ precache-manifest.8e021359cb1694eb7bbc5c2cee9fb068.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7739ed4abc51d5dfedfc022469f7148",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4dd57ca227c7cd4df560",
+    "url": "/robofriends/static/css/main.4b8cbf1e.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "4dd57ca227c7cd4df560",
+    "url": "/robofriends/static/js/main.3ab6b195.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
